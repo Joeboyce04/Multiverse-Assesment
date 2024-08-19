2b699dc6e1a1eb66ee302845560392fd3c1af0bf
Joseph Boyce <joseph.boyce@bjss.com>
2024-08-19 10:11:23 +0100

    updated code

--- main.go
+++ main.go
@@ -1,8 +1,53 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Client struct {
-	Name   string
-	Signed bool
-	Revenue int
+	Name           string
+	Revenue        int
+	ContractSigned bool
 }
 
+func FilterClientsByRevenue(clients []Client, minRevenue int) ([]Client, error) {
+	if minRevenue < 0 {
+		return nil, errors.New("revenue cannot be negative")
+	}
+
+var filtered []Client
+	for _, client := range clients {
+		if client.Revenue >= minRevenue {
+			filtered = append(filtered, client)
+		}
+	}
+	return filtered, nil
+}
+
+
+func FilterClientsBySignedStatus(clients []Client, signed bool) []Client {
+	var filtered []Client
+	for _, client := range clients {
+		if client.ContractSigned == signed {
+			filtered = append(filtered, client)
+		}
+	}
+	return filtered
+}
+
+func main() {
+	clients := []Client{
+		{"Client A", 10000, true},
+		{"Client B", 5000, false},
+		{"Client C", 15000, true},
+	}
+	filtered, _ := FilterClientsByRevenue(clients, 7000)
+	for _, client := range filtered {
+		fmt.Println(client.Name)
+	}
+	signedClients := FilterClientsBySignedStatus(clients, true)
+	for _, client := range signedClients {
+		fmt.Println(client.Name)
+	}
+}
--- main_test.go
+++ main_test.go
@@ -1,5 +1,75 @@
 package main
+
 import (
+	"errors"
 	"testing"
 )
 
+func TestFilterClientsByRevenue_ValidInput(t *testing.T) {
+	clients := []Client{
+		{"Client A", 10000, true},
+		{"Client B", 200, false},
+		{"Client C", 15000, true},
+	}
+
+	minRevenue := 1000
+
+	want := []Client{
+		{"Client A", 10000, true},
+		{"Client C", 15000, true},
+	}
+	
+	got, _ := FilterClientsByRevenue(clients, minRevenue)
+	
+	if len(got) != len(want) {
+		t.Error("got",len(got),"does not equal want",len(want))
+	}
+
+	for i, client := range got {
+		if client != want[i] {
+			t.Error("got",client," want ",want[i])
+		}
+	}
+}
+
+
+func TestFilterClientsByRevenue_NegativeRevenue(t *testing.T) {
+	
+	clients := []Client{
+		{"Client A", 10000, true},
+		{"Client B", 5000, false},
+	}
+	minRevenue := -5000
+	wantErr := errors.New("revenue cannot be negative")
+	
+	_, gotErr := FilterClientsByRevenue(clients, minRevenue)
+	
+	if gotErr == nil || gotErr.Error() != wantErr.Error() {
+		t.Error("got error",gotErr,"want error",wantErr,)
+	}
+}
+
+func TestFilterClientsBySignedStatus(t *testing.T) {
+	
+	clients := []Client{
+		{"Client A", 10000, true},
+		{"Client B", 5000, false},
+		{"Client C", 15000, true},
+	}
+	signed := true
+	want := []Client{
+		{"Client A", 10000, true},
+		{"Client C", 15000, true},
+	}
+	
+	got := FilterClientsBySignedStatus(clients, signed)
+	
+	if len(got) != len(want) {
+		t.Error("got",len(got)," clients want", len(want),)
+	}
+	for i, client := range got {
+		if client != want[i] {
+			t.Error("got",client,"want", want[i],)
+		}
+	}
+}
